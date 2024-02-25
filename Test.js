c9a9c9df4ac32a71e53005f4a1241580f4abdb8c
RousseauMalco <rousseaumalco@gmail.com>
2024-02-24 18:06:52 -0600

    Works, but not all groups filled.

--- Test.js
+++ Test.js
@@ -16,6 +16,8 @@ const names = ["Josie Melendez",
     "Eric Murphy",
     "Harley Bowman"];
 
+// const names = ["Henry", "Joe", "Jen", "carl", "Jason M. Oli"];
+
 const group1 = [];
 const group2 = [];
 const group3 = [];
@@ -23,23 +25,45 @@ const group4 = [];
 
 const groupGroup = [group1, group2, group3, group4];
 
-var tot_groups = 4;
-var max_size = tot_groups + names.length % tot_groups;
+let tot_groups = 4;
+let desired_size = 4;
 
-for (var i = 0; i < names.length; i++) {
-    var group = getRandomInt(4);
-    if (group == 0 && group1.length < 4) {
+// if (Math.floor(names.length % tot_groups < tot_groups) < ) {
+
+// }
+let max_size = desired_size + 1
+
+if (group1[1] != null) {
+    console.log("not reset");
+}
+
+for (let i = 0; i < names.length; i++) {
+    let position = getRandomInt(4);
+    console.log(position);
+
+    if (position == 0 && group1.length <= desired_size) {
+        console.log("iteration" + i + "i'm in this conditional 1");
         group1.push(names[i]);
-    } else if (group == 1 && group2.length < 4) {
+    } else if (position == 1 && group2.length <= desired_size) {
+        console.log("iteration" + i + "i'm in this conditional 2");
+
         group2.push(names[i]);
-    } else if (group == 2 && group3.length < 4) {
+    } else if (position == 2 && group3.length <= desired_size) {
+        console.log("iteration" + i + "i'm in this conditional 3");
+
         group3.push(names[i]);
-    } else if (group == 3 && group4.length < 4) {
+    } else if (position == 3 && group4.length <= desired_size) {
+        console.log("iteration" + i + "i'm in this conditional 4");
+
         group4.push(names[i]);
     } else {
-        for (var j = 0; j < groupGroup.length; j++) {
+
+        // for (let )
+        console.log("In else conditional at " + i + " iteration");
+        for (let j = 0; j < groupGroup.length; j++) {
             if (groupGroup[j].length < max_size) {
                 groupGroup[j].push(names[i]);
+                break;
             }
         }
     }
