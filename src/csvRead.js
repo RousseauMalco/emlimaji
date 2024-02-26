6d497b306cd311104ff317d863c167c46a093f07
Ensatsu <120747265+Ensatsu@users.noreply.github.com>
2024-02-26 16:17:25 -0600

    csvRead can read csv files but not properly

--- src/csvRead.js
+++ src/csvRead.js
@@ -1,13 +1,22 @@
+var fs = require("fs")
 let papaparse = require('papaparse')
+let file = "csvTest.csv"
 
-const csvData = "hi, hello, world, 1, 2, 3"
+var content = fs.readFileSync(file, "utf-8")
 
-console.log(papaparse.parse(csvData))
+var rows;
+papaparse.parse(content, {
+    download:false,
+    header: true,
+    delimiter: ",",
+    complete: function(results) {
+        //console.log("Finished:", results.data);
+        rows = results.data;
+    }
+});
+console.log("console log: ",rows);
+console.log(rows[0])
 
-// Papa.parse("csvTest.csv", {
-// 	complete: function(results) {
-// 		console.log(results);
-// 	}
-// });
-
-// Papa.parse(file, config)
+// for(var i=0; i < rows.length-1; i++){
+//     console.log(papaparse.parse(rows))
+// }
