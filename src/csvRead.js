727f3585b08746a069fd9d21c98a05f0a16a7839
Ensatsu <120747265+Ensatsu@users.noreply.github.com>
2024-02-27 15:50:09 -0600

    csvRead complete

--- src/csvRead.js
+++ src/csvRead.js
@@ -3,20 +3,22 @@ let papaparse = require('papaparse')
 let file = "csvTest.csv"
 
 var content = fs.readFileSync(file, "utf-8")
-
 var rows;
+var names = [];
+
 papaparse.parse(content, {
     download:false,
     header: false,
     delimiter: ",",
     complete: function(results) {
-        //console.log("Finished:", results.data);
         rows = results.data;
+        rows.shift(); // shift removes the first element and returns that value
+
+        for(var i=0; i < rows.length-1; i++){
+            names[i] = rows[i][1];
+        }
+        
     }
 });
-console.log("console log: ",rows);
-console.log(rows[1][1])
 
-// for(var i=0; i < rows.length-1; i++){
-//     console.log(papaparse.parse(rows))
-// }
+console.log(names)
