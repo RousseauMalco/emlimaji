f0a0082fcae041ff6895babcc47e936a52faa7c4
linhdieudang02 <115026636+linhdieudang02@users.noreply.github.com>
2024-04-09 16:20:13 -0500

    Update MakeTeamsButton.js

--- src/Components/MakeTeamsButton.js
+++ src/Components/MakeTeamsButton.js
@@ -32,12 +32,19 @@ export function MakeTeamsButton({inputNames,tot_group, option}) {
       const draggedMemberIndex = dragItem.current;
       const draggedMember = groups.flatMap(group => group)[draggedMemberIndex];
       const updatedGroups = [...groups];
-      // Remove the dragged member from its original group
-      const originalGroupIndex = updatedGroups.findIndex(group => group.includes(draggedMember));
-      const originalGroup = updatedGroups[originalGroupIndex];
+
+      let originalGroupIndex;
+      let originalGroup;
+      updatedGroups.forEach((group, index) => {
+      if (group.includes(draggedMember)) {
+        originalGroupIndex = index;
+        originalGroup = group;
+      }
+      });
       originalGroup.splice(originalGroup.indexOf(draggedMember), 1);
 
-      // Add the dragged member to the target group
+
+      updatedGroups[targetGroupIndex] = updatedGroups[targetGroupIndex] || [];
       updatedGroups[targetGroupIndex].push(draggedMember);
 
       setGroups(updatedGroups);
@@ -48,14 +55,14 @@ export function MakeTeamsButton({inputNames,tot_group, option}) {
       console.log("render called")
       if(props.groups && props.groups.length > 0) {
           return (
-            <ul class="space-y-5">
+            <ul class="space-y-5" onDragOver={(e) => dragOver(e)}>
               {
                 props.groups.map((group,groupIndex) =>
-                 <li class="ring-2 sm:rounded-lg relative bg-white px-3 pt-10 pb-8 shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg" key={groupIndex} id={`group-${groupIndex}`} onDragOver={(e) => dragOver(e)}> 
+                 <li class="ring-2 sm:rounded-lg relative bg-white px-3 pt-10 pb-8 shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg" key={groupIndex} id={`group-${groupIndex}`}  > 
                     Group: 
-                    <ul class="space-x-5">
-                      {group.map((member,memberIndex) => <li key={memberIndex} id={`member-${memberIndex}`} class="inline-block" draggable="true" onDragStart={(e) => dragStart(e, memberIndex)} onDragOver={dragOver}
-                            onDrop={(e) => drop(e,groupIndex)}> {member}
+                    <ul class="space-x-5" onDragOver={dragOver}
+                            onDrop={(e) => drop(e,groupIndex)}>
+                      {group.map((member,memberIndex) => <li key={memberIndex} id={`member:${memberIndex}`} class="inline-block" draggable="true" onDragStart={(e) => dragStart(e, memberIndex)} > {member}
                       </li>)}
                     </ul>
                  </li>
