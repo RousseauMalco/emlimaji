c18fb771f2e17ffd2c4bb20013eb24504cea076d
RousseauMalco <rousseaumalco@gmail.com>
2024-04-19 14:30:32 -0500

    Moved things to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import jimLogo from './Components/jimLogo.png';
-import React from 'react';
 import './App.css';
-import { FileUploadAndRead } from './Components/FileUploadAndRead';
 import { CSVTemplateLink } from './Components/CSVTemplateLink';
+import React, { useState } from 'react';
+import { FileUpload } from './Components/FileUpload';
+import { MakeTeamsButton } from './Components/MakeTeamsButton';
+import { GroupCustomizeTextField } from './Components/GroupCustomizeTextField';
+import { MultipleChoice } from './Components/GroupOrNumMem';
 
  
 function App() {
+  const [readData, setReadData] = useState('');
+  const [tot_group, setTotGroup] = useState(4);
+  const [selectedOption, setSelectedOption] = useState("Group Size");
+
+  const handleRead = (value) => {
+    setReadData(value);
+  };
+
+
   return (
     
     <div className="App">
@@ -36,11 +48,18 @@ function App() {
         <div className = "container-left">  
           <p className="text-lg text-gray-700 font-bold" style={{ whiteSpace: 'pre-wrap' }}>No CSV? Make a CSV here! </p>
           <CSVTemplateLink url = "https://drive.google.com/drive/folders/1p5LKcEG0COVpbGXtDMCPHTgQZdOk5osD?usp=sharing" />
+          <p className="text-1xl text-gray-700 font-bold">Got your CSV? Upload it here: </p>
+          <FileUpload onRead={handleRead}/>
+            <div class='flex flex-row m-5 mx-20'>
+                <div class='grow-0 m-1'>
+                    <MultipleChoice options={["Group Size","Number of groups"]} onAnswer={(option) => {setSelectedOption(option)}} > </MultipleChoice>  
+                </div>
+                <GroupCustomizeTextField inputSize={tot_group} onSizeChange={(newTotGroup) => setTotGroup(newTotGroup)} />
+            </div>
         </div>
         <div class="vl"></div>
         <div className = "container-right">
-          <p className="text-1xl text-gray-700 font-bold">Got your CSV? Upload it here: </p>
-          <FileUploadAndRead />
+          <MakeTeamsButton inputNames={readData} tot_group={tot_group} option ={selectedOption}/>     
         </div>
       </div>
       </body>
